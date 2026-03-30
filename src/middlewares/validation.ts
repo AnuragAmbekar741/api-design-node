eb03efd1bcf670bb7766adc79bfb4555052eb65c
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2026-03-30 11:30:28 -0700

    schema validation middleware

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -21,3 +21,21 @@ export const payloadValidation = (schema: ZodSchema) => {
     }
   }
 }
+
+export const paramValidation =
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.params)
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return res.status(400).json({
+          error: 'Invalid params',
+          details: err.issues.map((e) => ({
+            fields: e.path.join('.'),
+            details: e.message,
+          })),
+        })
+      }
+      next(err)
+    }
+  }
