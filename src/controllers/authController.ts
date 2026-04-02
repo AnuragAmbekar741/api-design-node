7fa5b8899e24e1f734e0dc25375c95c81853ee46
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2026-04-01 18:13:37 -0700

    register controller completed

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,42 @@
 import type { Request, Response } from 'express'
 import { db } from '../db/connection.ts'
-import { users } from '../db/schema.ts'
+import { users, type CreateUser } from '../db/schema.ts'
 import { generateToken } from '../utils/jwt.ts'
 import { hashedPassword } from '../utils/hashPassword.ts'
 
-export const register = (req: Request, res: Response) => {
+export const register = async (
+  req: Request<any, any, CreateUser>,
+  res: Response
+) => {
   try {
-  } catch (err) {}
+    const { email, username, password, firstName, lastName } = req.body
+    const encryptedPassword = await hashedPassword(password)
+    const [user] = await db
+      .insert(users)
+      .values({
+        email,
+        password: encryptedPassword,
+        username,
+        firstName,
+        lastName,
+      })
+      .returning({
+        id: users.id,
+        email: users.email,
+        username: users.username,
+        firstName: users.firstName,
+        lastName: users.lastName,
+        createAt: users.createAt,
+      })
+
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    })
+
+    return res.status(201).json({ message: 'User created', user, token: token })
+  } catch (err) {
+    console.error('Registration error')
+  }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
+import { register } from '../controllers/authController.ts'
+import { payloadValidation } from '../middlewares/validation.ts'
+import { insertUserScehma } from '../db/schema.ts'
 
 const router = Router()
 
-router.post('/register', (req, res) => {
-  res.status(201).json({
-    message: 'user signed up',
-  })
-})
+router.post('/register', payloadValidation(insertUserScehma), register)
 
 router.post('/login', (req, res) => {
   res.status(201).json({
